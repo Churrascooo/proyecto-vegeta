59fa1e12c037fcd1c1426b52ff25accf94f59a4c
Diego Valenzuela <163453007+claapii@users.noreply.github.com>
2024-06-08 15:31:44 +0000

    Like para que fernan lo vea

--- main.c
+++ main.c
@@ -4,7 +4,7 @@ int main(void) {
   printf("a");
   printf("prueba");
   printf("Hello World\n");
-  printf("Ariel es un tonto muy tonoto");
-  printf("Este cambio va para ariel carrasco");
+  printf("Ariel es un tonto muy tonoto\n");
+  printf("Hola soy Diego Valenzuela");
   return 0;
 }
