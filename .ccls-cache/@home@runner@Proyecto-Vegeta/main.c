cf8752c7e9bdfb08ee9eb2a1e030e979fb89d5f1
Churrascooo <163452323+Churrascooo@users.noreply.github.com>
2024-06-13 00:29:47 +0000

    Avance del menu principal (main)

--- .ccls-cache/@home@runner@Proyecto-Vegeta/main.c
+++ .ccls-cache/@home@runner@Proyecto-Vegeta/main.c
@@ -1,10 +1,57 @@
 #include <stdio.h>
+#include "tdas/map.h"
+#include "tdas/list.h"
 
-int main(void) {
-  printf("a");
-  printf("prueba");
-  printf("Hello World\n");
-  printf("Ariel es un tonto muy tonoto\n");
-  printf("Hola soy Diego Valenzuela");
+void mostrarMenuPrincipal()
+{
+  puts("----------------------------------------------");
+  puts("=== Bienvenidos a Vegeta Makes Everything! ===");
+  puts("----------------------------------------------");
+  puts("\n");
+
+  puts("1) Búsqueda de productos");
+  puts("2) Carrito de compras");
+  puts("3) Eliminar del carrito de compras");
+  puts("4) Comprobar compatibilidad");
+  puts("5) Salir del programa");
+  puts("\n");
+}
+
+
+int main(void)
+{
+  char opcion;
+
+  do
+  {
+    mostrarMenuPrincipal();
+    printf("Ingrese la opción que desea: ");
+    scanf(" %c", &opcion);
+
+    switch(opcion)
+    {
+      case '1':
+        //busquedaProducto();
+        break;
+      case '2':
+        //carritoDeCompras();
+        break;
+      case '3':
+        //eliminarProducto();
+        break;
+      case '4':
+        //comprobacionCompatibilidad();
+        break;
+      case '5':
+        puts("Saliendo del programa...");
+        puts("Gracias por usar Vegeta Makes Everything :]");
+        break;
+      default:
+        puts("Opcióno inválida, por favor ingrese una correcta.");
+        break;
+    }
+    
+  } while (opcion != '5');
+    
   return 0;
 }
